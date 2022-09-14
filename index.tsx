4bae7abf0b8cc83cff25b3b4e01559e02daf443d
Gepolter <73522230+Gepolter@users.noreply.github.com>
2022-09-14 20:27:23 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,17 +1,17 @@
 import dynamic from 'next/dynamic';
 import {FC, memo} from 'react';
 
-import Page from '../components/Layout/Page';
-import About from '../components/Sections/About';
+import Page from './src/components/Layout/Page';
+import About from './src/components/Sections/About';
 //import Contact from '../components/Sections/Contact';
-import Footer from '../components/Sections/Footer';
-import Hero from '../components/Sections/Hero';
+import Footer from './src/components/Sections/Footer';
+import Hero from './src/components/Sections/Hero';
 //import Portfolio from '../components/Sections/Portfolio';
-import Resume from '../components/Sections/Resume';
+import Resume from './src/components/Sections/Resume';
 //import Testimonials from '../components/Sections/Testimonials';
 
 // eslint-disable-next-line react-memo/require-memo
-const Header = dynamic(() => import('../components/Sections/Header'), {ssr: false});
+const Header = dynamic(() => import('./src/components/Sections/Header'), {ssr: false});
 
 const Home: FC = memo(() => {
   const title = 'Resume Dominik Heyer';
